f16e6e10ac0b5d93a7de340679ba4db88f63430c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-30 12:48:50 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ let searchForm = document.querySelector('.header .search-form');
 searchBtn.onclick = () => {
     searchBtn.classList.toggle('fa-times');
     searchForm.classList.toggle('active');
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    
 }
 
 /*Menu btn*/
@@ -14,4 +17,13 @@ let navbar = document.querySelector('.header .navbar');
 menuBtn.onclick = () => {
     menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('active');
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
 }
